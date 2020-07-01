137a8f7a5f28852bd3bdbd4dce0a8e9579136b99
sn <sebastianneumann@hotmail.com>
2020-07-01 09:11:46 +0200

    Updates

---
+++ precache-manifest.d2dc4fcf00995656c116e0a79a42be0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abeaafe25e42ff1ee78eaf515df295b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae4ad91451663a6e5b57",
+    "url": "/monsters-rolodex/static/css/main.d76c8398.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4ad91451663a6e5b57",
+    "url": "/monsters-rolodex/static/js/main.e00aeae9.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
